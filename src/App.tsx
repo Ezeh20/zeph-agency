7cd8b7a13d9e81e2aec2faff9ad3851db7c85f1a
Ezeh Chijioke <chijioke@clivealliance.com>
2024-01-02 21:44:54 +0100

    chore: routing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Services from "./pages/services";
+import Work from "./pages/work";
+import Contact from "./pages/contact";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/work" element={<Work />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
